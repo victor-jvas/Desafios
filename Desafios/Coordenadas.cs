27a24da202c97f94bbba5e1bdd4170d9b1d2f377
jVictor <victor.jvas1@gmail.com>
2021-06-18 22:52:14 -0300

    Initial commit

---
+++ Desafios/Coordenadas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Desafios
+{
+    public class Coordenadas
+    {
+        public void Coord()
+        {
+            double x, y;
+        
+            var input = Console.ReadLine();
+        
+            var values = input.Split(" ");
+        
+            x = double.Parse(values[0]);
+            y = double.Parse(values[1]);
+        
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+        
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+        
+            if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+        
+            if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+        
+            if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+        
+            if (x == 0 && y != 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+        
+            if (x != 0 && y == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+        }
+        
+    }
+}
