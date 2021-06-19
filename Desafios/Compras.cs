af40612f0323102c4f462aa1b0a0e8f849081c14
jVictor <victor.jvas1@gmail.com>
2021-06-18 22:54:23 -0300

    Compras no Supermercado

---
+++ Desafios/Compras.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafios
+{
+    public class Compras
+    {
+        public void Comp()
+        {
+            var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < totalDeCasosDeTeste; i++)
+            {
+                var list = Console.ReadLine().Split();
+                var orderedList = list.OrderBy(x=>x);
+                var finalList = new HashSet<string>(orderedList);
+
+                var count = 0;
+                foreach (var item in finalList)
+                {
+                    if (count == 0)
+                    {
+                        Console.Write(item);
+                    }
+                    else
+                    {
+                        Console.Write(" " + item);    
+                    }
+
+                    count++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Desafios/Program.cs
+++ Desafios/Program.cs
@@ -8,30 +8,7 @@ namespace Desafios
     {
         static void Main(string[] args)
         {
-            var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
-
-            for (var i = 0; i < totalDeCasosDeTeste; i++)
-            {
-                var list = Console.ReadLine().Split();
-                var orderedList = list.OrderBy(x=>x);
-                var finalList = new HashSet<string>(orderedList);
-
-                var count = 0;
-                foreach (var item in finalList)
-                {
-                    if (count == 0)
-                    {
-                        Console.Write(item);
-                    }
-                    else
-                    {
-                        Console.Write(" " + item);    
-                    }
-
-                    count++;
-                }
-                Console.WriteLine();
-            }
+            
         }
     }
 }
